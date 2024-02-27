b53dfac48423da474a07852efe586c8fa1baf248
BYCode-Infotech <codedatayash0@gmail.com>
2024-02-27 22:18:25 +0530

    creating routing...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import './styles/index.scss';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 
 function App() {
   return (
     <div>
-
+      <RouterProvider router={router} />
     </div>
   );
 }
