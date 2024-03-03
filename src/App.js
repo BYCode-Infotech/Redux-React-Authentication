d4b49381534d3100ef46736786548002eae0e67f
BYCode-Infotech <codedatayash0@gmail.com>
2024-03-03 12:55:21 +0530

    add login pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { Provider } from 'react-redux';
 import './App.scss';
 import router from './router';
 import './styles/index.scss';
 import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import store from './redux/store';
 
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <Provider store={store}>
+      <div>
+        <RouterProvider router={router} />
+        <Toaster />
+      </div>
+    </Provider>
   );
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Defaultlayout from "./defaultlayout";
+import SignUp from "../form/signup";
+import Login from "../form/login/login";
+import Home from "../app/home";
 
 
 
 const router = createBrowserRouter([
     {
+        path: '/',
+        element: localStorage.getItem('loggedIn') === 'true' ? <Navigate to="/home" /> : <SignUp />
+    },
+    {
+        path: '/login',
+        element: localStorage.getItem('loggedIn') === 'true' ? <Navigate to="/home" /> : <Login />,
+    },
+    {
         path: "/",
         element: <Defaultlayout />,
+        children: [
+            {
+                path: "/home",
+                element:
+                    localStorage.getItem('loggedIn') === 'true' ? <Home /> : <Navigate to="/login" />
+            },
+        ]
     }
 ])
 
