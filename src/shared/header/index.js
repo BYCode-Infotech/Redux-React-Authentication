b10baab1d5b92b74c55de28e503f6b75583f6820
BYCode-Infotech <codedatayash0@gmail.com>
2024-03-03 12:26:41 +0530

    make header...

--- src/shared/header/index.js
+++ src/shared/header/index.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import './header.scss'
 import logo from '../../assets/logo/logo.png'
+import { CgMenuRight } from "react-icons/cg";
 
 export default function Header() {
   return (
     <div className='header-main'>
       <div className="container">
-        <Header>
+        <header>
           <div className="logo">
             <img src={logo} alt="logo-png" />
           </div>
-        </Header>
+          <div className="menu">
+            <div className="menu-items">
+              <a href="HOME">Home</a>
+            </div>
+            <div className="menu-items">
+              <a href="SERVICES">Services</a>
+            </div>
+            <div className="menu-items">
+              <a href="COMPANY">Our Company</a>
+            </div>
+            <div className="menu-items">
+              <a href="HIRE US">Hire us</a>
+            </div>
+            <CgMenuRight className='menu-icon' />
+          </div>
+        </header>
       </div>
     </div>
   )
